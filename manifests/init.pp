0e58b5954f4ac29ebb63b145c5e79af83e0d60ea
Jeff McCune <jeff@puppetlabs.com>
2011-03-24 13:16:48 -0700

    Initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: mcollective
+#
+# This module manages mcollective
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class mcollective {
+
+
+}
