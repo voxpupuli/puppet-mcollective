6ebb6f22dd8f16b0ede9866b4c7ddd8fd4276eab
Jeff McCune <jeff@puppetlabs.com>
2011-05-30 11:01:15 -0700

    Fix: package should come before the configuration
    There was a problem where the configuration file could not be managed
    since the parent directory did not yet exist.  Ensuring the package is
    installed before the configuration file resolves this problem.

--- manifests/client/base.pp
+++ manifests/client/base.pp
@@ -5,14 +5,14 @@ class mcollective::client::base(
   $pkg_provider = $mcollective::params::pkg_provider
 ) inherits mcollective::params {
 
-  class { 'mcollective::client::config':
-    config      => $config,
-    config_file => $config_file,
-  }
   class { 'mcollective::client::pkg':
     version      => $version,
     pkg_provider => $pkg_provider,
-    require      => Class['mcollective::client::config'],
+  }
+  class { 'mcollective::client::config':
+    config      => $config,
+    config_file => $config_file,
+    require     => Class['mcollective::client::pkg'],
   }
 
 }
