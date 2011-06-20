9a15a32856c9864cb21eced695409f9f5f1368d4
Jeff McCune <jeff@puppetlabs.com>
2011-06-20 13:55:45 -0700

    (#8) Update example with end-user expected use
    The example in the tests/ directory is now updated with the expected use
    case of the end user of the module.  This include setting up an
    MCollective client and server using the Java and ActiveMQ modules.

--- tests/init.pp
+++ tests/init.pp
@@ -3,9 +3,23 @@ node default {
   # The default behavior is to configure the server only and not the client.
   # The following PSK and Server settings are the defaults, but you may
   # want to change them for your site.
+  notify { "alpha":
+    message => "alpha",
+  }
+  ->
+  class { 'java': }
+  ->
+  class { 'activemq': }
+  ->
   class { 'mcollective':
     mc_security_psk => 'changemeplease',
     stomp_server    => 'stomp',
+    server          => true,
+    client          => true,
+  }
+  ->
+  notify { "omega":
+    message => "omega",
   }
 
 }
