123308a8abe77e9765da346d6207e91f770c54b8
Richard Clamp <richardc@unixbeard.net>
2013-09-27 11:38:28 +0100

    (#56) notify service to restart on plugin install
    See if we're managing a server, and if so set notifications from the package
    resource or File[$mcollective::site_libdir].
    
    This isn't excercised by the rspec-puppet tests, as there's no way
    to test for resource relationships currently with rspec-puppet.
    Maybe if rodjek/rspec-puppet#106 were merged.

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -4,14 +4,21 @@ define mcollective::plugin(
   $package = false,
   $type = 'agent',
   $has_client = true,
-  # $client is to allow for unit testing, and considered a private
-  # parameter
+  # $client and $server are to allow for unit testing, and are considered private
+  # parameters
   $client = $mcollective::client,
+  $server = $mcollective::server,
 ) {
   if $package {
-    # install from a package named "mcollective-${name}-${type}
-    package { "mcollective-${name}-${type}":
+    # install from a package named "mcollective-${name}-${type}"
+    $package_name = "mcollective-${name}-${type}"
+    package { $package_name:
       ensure => 'present',
+    }
+
+    if $server {
+      # set up a notification if we know we're managing a server
+      Package[$package_name] ~> Class['mcollective::server::service']
     }
 
     # install the client package if we're installing on a $mcollective::client
