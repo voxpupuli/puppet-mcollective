32830b4c87f3a8b0ea8ccb2686f6ecb2ad316d4c
Alex Crowe <alex@pancentric.com>
2013-10-16 17:34:57 +0100

    typo in param for reusing ssl from agent

--- manifests/server/config/connector/activemq/hosts_iteration.pp
+++ manifests/server/config/connector/activemq/hosts_iteration.pp
@@ -2,7 +2,7 @@
 # $name will be an index into the $mcollective::middleware_hosts array + 1
 define mcollective::server::config::connector::activemq::hosts_iteration {
   if $mcollective::middleware_ssl {
-    if $mcollective::server_reuse_agent_ssl {
+    if $mcollective::server_use_agent_ssl {
       $cert = $mcollective::server_ssl_cert
       $key  = $mcollective::server_ssl_key
     }
