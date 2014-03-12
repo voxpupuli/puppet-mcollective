6a3202a1669a78d2221e2db86dffc1cff1972573
cristi1979 <cristi.falcas@gmail.com>
2014-03-12 12:47:20 +0200

    Update install.pp

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -7,13 +7,14 @@ class mcollective::server::install {
   if $mcollective::manage_packages {
     package { 'mcollective': ensure => $mcollective::version, }
 
+    $ruby_stomp_package = undef
+
     case $::osfamily {
       'Debian' : { $ruby_stomp_package = 'ruby-stomp' }
       'Redhat' : { $ruby_stomp_package = 'rubygem-stomp' }
-      default  : { $ruby_stomp_package = undef }
     }
 
-    if defined $ruby_stomp_package {
+    if $ruby_stomp_package {
       package { $ruby_stomp_package:
         ensure => $mcollective::ruby_stomp_ensure,
         before => Package['mcollective'],
