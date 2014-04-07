f57fd2e2920da45814cc65437fb29db7ac9d077e
renanvicente <renanvice@gmail.com>
2014-04-07 11:49:14 -0300

    Update install.pp
    Add a support to manage puppet agent by mcollective

--- manifests/client/install.pp
+++ manifests/client/install.pp
@@ -3,9 +3,9 @@ class mcollective::client::install {
   if $caller_module_name != $module_name {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
-
+  $mcollective_packages = ['mcollective-client','mcollective-puppet-agent','mcollective-puppet-client']
   if $mcollective::manage_packages {
-    package { 'mcollective-client':
+    package { $mcollective_packages:
       ensure => $mcollective::version,
     }
   }
