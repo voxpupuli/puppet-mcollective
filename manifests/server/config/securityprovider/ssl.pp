5d3b9f5b856105f1db981753bffdf3e1610721aa
Rafael Durán Castañeda <rafael.duran@ntt.eu>
2014-05-12 16:58:20 +0200

    Configuration directory is configurable

--- manifests/server/config/securityprovider/ssl.pp
+++ manifests/server/config/securityprovider/ssl.pp
@@ -4,7 +4,7 @@ class mcollective::server::config::securityprovider::ssl {
     fail("Use of private class ${name} by ${caller_module_name}")
   }
 
-  file { '/etc/mcollective/clients':
+  file { "${mcollective::confdir}/clients":
     ensure  => 'directory',
     owner   => 'root',
     group   => '0',
@@ -15,14 +15,14 @@ class mcollective::server::config::securityprovider::ssl {
   }
 
   mcollective::server::setting { 'plugin.ssl_client_cert_dir':
-    value => '/etc/mcollective/clients',
+    value => "${mcollective::confdir}/clients",
   }
 
   mcollective::server::setting { 'plugin.ssl_server_public':
-    value => '/etc/mcollective/server_public.pem',
+    value => "${mcollective::confdir}/server_public.pem",
   }
 
   mcollective::server::setting { 'plugin.ssl_server_private':
-    value => '/etc/mcollective/server_private.pem',
+    value => "${mcollective::confdir}/server_private.pem",
   }
 }
