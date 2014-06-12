reidmv/puppetlabs-mcollective

Make client-package name configurable

manifests/client/install.pp
@@ -5,7 +5,7 @@ class mcollective::client::install {
   }
 
   if $mcollective::manage_packages {
-    package { 'mcollective-client':
+    package { $mcollective::client_package:
       ensure => $mcollective::version,
     }
   }

voxpupuli/puppet-mcollective

manifests/client/install.pp
@@ -5,7 +5,7 @@ class mcollective::client::install {
   }
 
   if $mcollective::manage_packages {
-    package { 'mcollective-client':
+    package { $mcollective::client_package_name:
       ensure => $mcollective::version,
     }
   }