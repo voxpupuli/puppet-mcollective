reidmv/puppetlabs-mcollective

Propogate $mcollective::configdir
Remove further hard-coding of /etc/mcollective

manifests/actionpolicy.pp
@@ -7,7 +7,7 @@ define mcollective::actionpolicy($default = 'deny') {
     owner    => 'root',
     group    => '0',
     mode     => '0400',
-    path     => "/etc/mcollective/policies/${name}.policy",
+    path     => "${mcollective::configdir}/policies/${name}.policy",
     template => 'mcollective/actionpolicy.erb',
   }
 

manifests/common/config/connector/activemq/hosts_iteration.pp
@@ -1,8 +1,9 @@
 # private define
 # $name will be an index into the $mcollective::middleware_hostsarray + 1
 define mcollective::common::config::connector::activemq::hosts_iteration {
+  $middleware_hosts_array = flatten([$mcollective::middleware_hosts])
   mcollective::common::setting { "plugin.activemq.pool.${name}.host":
-    value => $mcollective::middleware_hosts[$name - 1], # puppet array 0-based
+    value => $middleware_hosts_array[$name - 1],
   }
 
   $port = $mcollective::middleware_ssl ? {
@@ -33,7 +34,7 @@ define mcollective::common::config::connector::activemq::hosts_iteration {
     }
 
     mcollective::common::setting { "plugin.activemq.pool.${name}.ssl.ca":
-      value => '/etc/mcollective/ca.pem',
+      value => "${mcollective::configdir}/ca.pem",
     }
 
     mcollective::common::setting { "plugin.activemq.pool.${name}.ssl.fallback":

manifests/common/config/connector/rabbitmq/hosts_iteration.pp
@@ -28,7 +28,7 @@ define mcollective::common::config::connector::rabbitmq::hosts_iteration {
     }
 
     mcollective::common::setting { "plugin.rabbitmq.pool.${name}.ssl.ca":
-      value => '/etc/mcollective/ca.pem',
+      value => "${mcollective::configdir}/ca.pem",
     }
 
     mcollective::common::setting { "plugin.rabbitmq.pool.${name}.ssl.fallback":

manifests/server/config/connector/activemq/hosts_iteration.pp
@@ -3,11 +3,11 @@
 define mcollective::server::config::connector::activemq::hosts_iteration {
   if $mcollective::middleware_ssl {
     mcollective::server::setting { "plugin.activemq.pool.${name}.ssl.cert":
-      value => '/etc/mcollective/server_public.pem',
+      value => "${mcollective::configdir}/server_public.pem",
     }
 
     mcollective::server::setting { "plugin.activemq.pool.${name}.ssl.key":
-      value => '/etc/mcollective/server_private.pem',
+      value => "${mcollective::configdir}/server_private.pem",
     }
   }
 }

manifests/server/config/connector/rabbitmq/hosts_iteration.pp
@@ -3,11 +3,11 @@
 define mcollective::server::config::connector::rabbitmq::hosts_iteration {
   if $mcollective::middleware_ssl {
     mcollective::server::setting { "plugin.rabbitmq.pool.${name}.ssl.cert":
-      value => '/etc/mcollective/server_public.pem',
+      value => "${mcollective::configdir}/server_public.pem",
     }
 
     mcollective::server::setting { "plugin.rabbitmq.pool.${name}.ssl.key":
-      value => '/etc/mcollective/server_private.pem',
+      value => "${mcollective::configdir}/server_private.pem",
     }
   }
 }

voxpupuli/puppet-mcollective

manifests/actionpolicy.pp
@@ -7,7 +7,7 @@ define mcollective::actionpolicy($default = 'deny') {
     owner    => 'root',
     group    => '0',
     mode     => '0400',
-    path     => "/etc/mcollective/policies/${name}.policy",
+    path     => "${mcollective::confdir}/policies/${name}.policy",
     template => 'mcollective/actionpolicy.erb',
   }
 

manifests/common/config/connector/activemq/hosts_iteration.pp
@@ -1,8 +1,9 @@
 # private define
 # $name will be an index into the $mcollective::middleware_hostsarray + 1
 define mcollective::common::config::connector::activemq::hosts_iteration {
+  $middleware_hosts_array = flatten([$mcollective::middleware_hosts])
   mcollective::common::setting { "plugin.activemq.pool.${name}.host":
-    value => $mcollective::middleware_hosts[$name - 1], # puppet array 0-based
+    value => $middleware_hosts_array[$name - 1],
   }
 
   $port = $mcollective::middleware_ssl ? {
@@ -33,7 +34,7 @@ define mcollective::common::config::connector::activemq::hosts_iteration {
     }
 
     mcollective::common::setting { "plugin.activemq.pool.${name}.ssl.ca":
-      value => '/etc/mcollective/ca.pem',
+      value => "${mcollective::confdir}/ca.pem",
     }
 
     mcollective::common::setting { "plugin.activemq.pool.${name}.ssl.fallback":

manifests/common/config/connector/rabbitmq/hosts_iteration.pp
@@ -28,7 +28,7 @@ define mcollective::common::config::connector::rabbitmq::hosts_iteration {
     }
 
     mcollective::common::setting { "plugin.rabbitmq.pool.${name}.ssl.ca":
-      value => '/etc/mcollective/ca.pem',
+      value => "${mcollective::confdir}/ca.pem",
     }
 
     mcollective::common::setting { "plugin.rabbitmq.pool.${name}.ssl.fallback":

manifests/server/config/connector/activemq/hosts_iteration.pp
@@ -3,11 +3,11 @@
 define mcollective::server::config::connector::activemq::hosts_iteration {
   if $mcollective::middleware_ssl {
     mcollective::server::setting { "plugin.activemq.pool.${name}.ssl.cert":
-      value => '/etc/mcollective/server_public.pem',
+      value => "${mcollective::confdir}/server_public.pem",
     }
 
     mcollective::server::setting { "plugin.activemq.pool.${name}.ssl.key":
-      value => '/etc/mcollective/server_private.pem',
+      value => "${mcollective::confdir}/server_private.pem",
     }
   }
 }

manifests/server/config/connector/rabbitmq/hosts_iteration.pp
@@ -3,11 +3,11 @@
 define mcollective::server::config::connector::rabbitmq::hosts_iteration {
   if $mcollective::middleware_ssl {
     mcollective::server::setting { "plugin.rabbitmq.pool.${name}.ssl.cert":
-      value => '/etc/mcollective/server_public.pem',
+      value => "${mcollective::confdir}/server_public.pem",
     }
 
     mcollective::server::setting { "plugin.rabbitmq.pool.${name}.ssl.key":
-      value => '/etc/mcollective/server_private.pem',
+      value => "${mcollective::confdir}/server_private.pem",
     }
   }
 }