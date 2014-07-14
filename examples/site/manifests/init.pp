5593ad888a294bebda2a0c14f519894897c941eb
Reid Vandewiele <reid@puppetlabs.com>
2014-07-14 16:07:13 -0700

    Add rabbitmq broker example

--- examples/site/manifests/init.pp
+++ examples/site/manifests/init.pp
@@ -2,16 +2,19 @@
 # together the MCollective middleware.
 #
 class site (
-  $main_collective     = undef,
-  $collectives         = undef,
-  $middleware_hosts    = undef,
-  $middleware_username = undef,
-  $middleware_password = undef,
-  $middleware_ssl_port = undef,
-  $ssl_server_public   = undef,
-  $ssl_server_private  = undef,
-  $ssl_server_ca       = undef,
-  $connector           = undef,
+  $main_collective           = undef,
+  $collectives               = undef,
+  $middleware_hosts          = undef,
+  $middleware_username       = undef,
+  $middleware_password       = undef,
+  $middleware_ssl_port       = undef,
+  $ssl_server_public         = undef,
+  $ssl_server_private        = undef,
+  $ssl_server_ca             = undef,
+  $connector                 = undef,
+  $middleware_admin_user     = undef,
+  $middleware_admin_password = undef,
+  $rabbitmq_vhost            = '/mcollective',
 ) {
 
   # No resources are declared by this class.
--- examples/site/manifests/rabbitmq_broker.pp
+++ examples/site/manifests/rabbitmq_broker.pp
@@ -1,5 +1,84 @@
-class example::rabbitmq_broker (
+class site::rabbitmq_broker (
+  $delete_guest_user = false,
+  $rabbitmq_confdir  = '/etc/rabbitmq',
+) inherits site {
 
-) {
+  file { "${rabbitmq_confdir}/ca.pem":
+    owner  => 'rabbitmq',
+    group  => 'rabbitmq',
+    mode   => '0444',
+    source => $site::ssl_ca_cert,
+  }
+
+  file { "${rabbitmq_confdir}/server_public.pem":
+    owner  => 'rabbitmq',
+    group  => 'rabbitmq',
+    mode   => '0444',
+    source => $site::ssl_server_public,
+  }
+
+  file { "${rabbitmq_confdir}/server_private.pem":
+    owner  => 'rabbitmq',
+    group  => 'rabbitmq',
+    mode   => '0400',
+    source => $site::ssl_server_private,
+  }
+
+  class { '::rabbitmq':
+    config_stomp      => true,
+    delete_guest_user => $delete_guest_user,
+    ssl               => true,
+    stomp_port        => $site::middleware_port,
+    ssl_stomp_port    => $site::middleware_ssl_port,
+    ssl_cacert        => "${rabbitmq_confdir}/ca.pem",
+    ssl_cert          => "${rabbitmq_confdir}/server_public.pem",
+    ssl_key           => "${rabbitmq_confdir}/server_private.pem",
+  }
+
+  rabbitmq_plugin { 'rabbitmq_stomp':
+    ensure => present,
+  } ->
+
+  rabbitmq_vhost { $site::rabbitmq_vhost:
+    ensure => present,
+  } ->
+
+  rabbitmq_user { $site::middleware_user:
+    ensure   => present,
+    admin    => false,
+    password => $site::middleware_password,
+  } ->
+
+  rabbitmq_user { $site::middleware_admin_user:
+    ensure   => present,
+    admin    => true,
+    password => $site::middleware_admin_password,
+  } ->
+
+  rabbitmq_user_permissions { "${site::middleware_user}@${site::rabbitmq_vhost}":
+    configure_permission => '.*',
+    read_permission      => '.*',
+    write_permission     => '.*',
+  } ->
+
+  rabbitmq_user_permissions { "${site::middleware_admin_user}@${site::rabbitmq_vhost}":
+    configure_permission => '.*',
+  } ->
+
+  rabbitmq_exchange { "mcollective_broadcast@${site::rabbitmq_vhost}":
+    ensure   => present,
+    type     => 'topic',
+    user     => $site::middleware_admin_user,
+    password => $site::middleware_admin_password,
+  } ->
+
+  rabbitmq_exchange { "mcollective_directed@${site::rabbitmq_vhost}":
+    ensure   => present,
+    type     => 'direct',
+    user     => $site::middleware_admin_user,
+    password => $site::middleware_admin_password,
+  }
+
+  contain rabbitmq
 
 }
