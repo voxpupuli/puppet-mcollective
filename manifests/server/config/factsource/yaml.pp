a4639d2280d5f1dfe2d4ddc72c96df2117b78994
Pieter Krul <github@krul.cc>
2015-03-25 02:05:21 +0100

    Space out refresh-mcollective-metadata cron task

--- manifests/server/config/factsource/yaml.pp
+++ manifests/server/config/factsource/yaml.pp
@@ -7,6 +7,9 @@ class mcollective::server::config::factsource::yaml {
   $excluded_facts      = $mcollective::excluded_facts
   $yaml_fact_path_real = $mcollective::yaml_fact_path_real
 
+  $cron_minute_value   = fqdn_rand(60, ${::macaddress})
+  $cron_hour_value     = '0-23/2'
+
   # Template uses:
   #   - $yaml_fact_path_real
   file { "${mcollective::core_libdir}/refresh-mcollective-metadata":
@@ -20,7 +23,8 @@ class mcollective::server::config::factsource::yaml {
     environment => "PATH=/opt/puppet/bin:${::path}",
     command     => "${mcollective::core_libdir}/refresh-mcollective-metadata",
     user        => 'root',
-    minute      => [ '0', '15', '30', '45' ],
+    minute      => $cron_minute_value,
+    hour        => $cron_hour_value,
   }
   exec { 'create-mcollective-metadata':
     path    => "/opt/puppet/bin:${::path}",
