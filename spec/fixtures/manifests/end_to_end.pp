b1d273e98ae6aa8fafe7de19c03919a480c0770b
Tim Meusel <tim@bastelfreak.de>
2016-09-08 12:06:45 +0200

    purge legacy code

--- spec/fixtures/manifests/end_to_end.pp
+++
@@ -1,5 +0,0 @@
-class { '::site_mcollective': }
-class { '::site_mcollective::client': }
-class { '::site_mcollective::server': }
-class { '::site_nagios::server': }
-class { '::site_nagios::mcollective': }
