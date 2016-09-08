0dca6a9149b3a54dadc3a46e976512de18a491ec
Tim Meusel <tim@bastelfreak.de>
2016-09-08 11:54:22 +0200

    puppet-lint: fix relative_classname_inclusion

--- examples/ssl_example/mco_profile/manifests/middleware/activemq.pp
+++ examples/ssl_example/mco_profile/manifests/middleware/activemq.pp
@@ -34,7 +34,7 @@ class mco_profile::middleware::activemq (
     instance      => 'mcollective',
     server_config => template('mco_profile/activemq_template.erb'),
   }
-  contain 'activemq'
+  contain '::activemq'
 
   # Set up SSL configuration. Use copies of the PEM keys specified to create
   # the Java keystores.
