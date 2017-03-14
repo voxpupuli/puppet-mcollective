274db99d423f0a8745a24d84b0f8aa9c40f636f5
Tim Meusel <tim@bastelfreak.de>
2017-03-14 10:19:45 +0100

    add datatypes to all params in rule.pp

--- manifests/actionpolicy/rule.pp
+++ manifests/actionpolicy/rule.pp
@@ -1,11 +1,11 @@
 # Define - mcollective::actionpolicy::rule
 define mcollective::actionpolicy::rule(
-  $agent,
-  $action      = 'allow',
-  $callerid    = '*',
-  $actions     = '*',
-  String $fact_filter = '*',
-  $classes     = '*'
+  String $agent,
+  Enum['allow', 'deny'] $action = 'allow',
+  String $callerid              = '*',
+  String $actions               = '*',
+  String $fact_filter           = '*',
+  String $classes               = '*'
 ) {
   datacat_fragment { "mcollective::actionpolicy::rule ${title}":
     target => "mcollective::actionpolicy ${agent}",
